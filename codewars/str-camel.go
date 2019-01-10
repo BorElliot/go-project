9d2783e6e29a500952934f498f39f9a457d5a8e3
arno.xu <arno.xu@etocrm.com>
2019-01-10 11:22:31 +0800

    camel string

--- codewars/str-camel.go
+++ codewars/str-camel.go
@@ -3,17 +3,20 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"strings"
 )
 
 var re = regexp.MustCompile("(_|-)([a-zA-Z]+)")
 
 func ToCamelCase(str string) string {
+	str = strings.Title(str)
+	fmt.Println(str)
 	camel := re.ReplaceAllString(str, "$2")
 
 	return camel
 }
 
 func main() {
-	str := "The_Stealth_Warrior"
+	str := "The-Stealth-Warrior"
 	fmt.Println(ToCamelCase(str))
 }
